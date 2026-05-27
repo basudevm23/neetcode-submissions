622ef4600b442fc7386d0efdb62edf9f448c7232
Basudev Mohapatra <166227341+basudevm23@users.noreply.github.com>
2026-05-27 14:17:56 +0530

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if(height.empty()){
+            return 0;
+        }
+        int n = height.size();
+        int left= 0;
+        int right = n-1;
+        int area_water = 0;
+        int leftmax = height[0];
+        int rightmax = height[n-1];
+        
+        while(left<right){
+            if(leftmax<rightmax){
+                area_water += leftmax - height[left];
+                left++;
+                leftmax = max(leftmax, height[left]);
+                // area_water += leftmax - height[left];
+            }
+            else{
+                area_water += rightmax - height[right];
+                right--;
+                rightmax = max(rightmax, height[right]);
+                // area_water += rightmax - height[right];
+            }
+        }
+        return area_water;
+    }
+};
