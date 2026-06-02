d8264f1c7268b109b163d8dc9cd76752deae4cbc
Basudev Mohapatra <166227341+basudevm23@users.noreply.github.com>
2026-06-02 20:40:33 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,19 @@
+class MinStack {
+private:
+    vector<int> ans;
+public:
+    MinStack() {
+    }  
+    void push(int val) {
+        ans.push_back(val);             
+    } 
+    void pop() {
+        ans.pop_back();           
+    }
+    int top() {
+        return ans.back();   
+    }
+    int getMin() {
+        return *min_element(ans.begin(),ans.end());     
+    }
+};
