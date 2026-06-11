8aad6c483ac0d3db53cc989a78ebd4b78e48fcfc
Basudev Mohapatra <166227341+basudevm23@users.noreply.github.com>
2026-06-11 11:49:16 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // nlogn with o1 and 
+        int n =nums.size();
+        for(int i=0; i<n;i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i]==nums[j]){
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+};
