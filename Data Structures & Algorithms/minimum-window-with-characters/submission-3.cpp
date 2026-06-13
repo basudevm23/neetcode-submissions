e3e06212970b102e45bc819865dc7c678ea73103
Basudev Mohapatra <166227341+basudevm23@users.noreply.github.com>
2026-06-13 20:14:45 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        // we have to keep two hasharrays
+        // also for haves and needs;
+        if(s.size()<t.size()) return "";
+        if(s == t) return s;
+        int l=0;
+        int r = 0;
+        int minstr=INT_MAX;
+        // correctly initialize it
+        int leftindex= 0 ;
+        int have = 0;
+        int need=  0;
+        unordered_map<char, int> hashs;
+        unordered_map<char, int> hasht;
+
+        for(int i=0; i<t.size(); i++){
+            if(hasht[t[i]]==0){
+                need++;
+            }
+            hasht[t[i]]++;
+        }
+
+        while(l<=r&&r<s.size()){
+
+            if(hasht.find(s[r])!=hasht.end()){
+                hashs[s[r]]++;
+                if(hashs[s[r]]==hasht[s[r]]){
+                    have++;
+                }
+            }
+            while(have==need && l<=r){
+                // first record the answer and then update it
+                if(r-l+1 <minstr){
+                    minstr = r-l+1;
+                    leftindex = l;
+                }
+                if(hasht.find(s[l])!=hasht.end() && hashs[s[l]]==hasht[s[l]]){
+                    have--;
+                }
+                if(hashs.find(s[l])!=hashs.end()){
+                    hashs[s[l]]--;
+                }            
+                
+                l++;
+            }
+            r++;                    
+        }
+        // if s = a and t = "b" then this should be the answer
+        if(minstr ==INT_MAX){
+            return "";
+        }
+        return s.substr(leftindex, minstr);
+    }
+};
