56292184f629eafb085aea9f9e9f5c92d4867d7d
Basudev Mohapatra <166227341+basudevm23@users.noreply.github.com>
2026-07-20 01:50:42 +0530

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        prev1 = 1
+        prev2 = 1
+        prev3 = 0
+        if n == 0:
+            return prev3
+        if n==1:
+            return prev2
+        if n == 2:
+            return prev1
+
+        for i in range(2, n):
+            curr = prev1 + prev2 + prev3
+            prev3 = prev2
+            prev2 = prev1
+            prev1 = curr
+        return prev1
