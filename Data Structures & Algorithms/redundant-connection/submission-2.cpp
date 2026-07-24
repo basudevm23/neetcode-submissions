83c6b2bfdc63dc639707fb3d2dc9b0ba4f566e5c
Basudev Mohapatra <166227341+basudevm23@users.noreply.github.com>
2026-07-25 02:52:50 +0530

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+
+        // a tree cannot contain a cycle, while adding edges one by one, the first edge
+class Solution {
+public:
+    bool dfs(int node, int parent, vector<bool>& vis, vector<vector<int>>& adj){
+        if(vis[node]) return true;
+        vis[node]=true;
+        for(auto neighbor: adj[node]){
+            if(parent == neighbor){
+                continue;
+            }
+            if(dfs(neighbor, node, vis, adj)){
+                return true;
+            }
+        }
+        return false;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+
+        vector<vector<int>> adj(n+1);
+
+        for(int i=0; i<edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+
+            vector<bool> vis(n+1, -1);
+
+            if(dfs(u, -1, vis, adj)){
+                return {u, v};
+            }
+        }
+        return {-1, -1};
+    }
+};
+        vector<vector<int>> adj(edges.size()+1);
+        
+        for(int i=0; i<edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+
+            vector<bool> vis(edges.size()+1, false);
+
+            if(dfs(u, -1, vis, adj)){
+                return {u, v};
+            }
+        }
+        
+    }
+
+    bool dfs(int node, int parent, vector<bool>& vis, vector<vector<int>>& adj){
+        if(vis[node]) return true;
+        vis[node] = true;
+        for(auto nei: adj[node]){
+            if(nei == parent) continue;
+            if(dfs(nei, node, vis, adj)) return true;
+        }
+        return false;
+    }
+};
