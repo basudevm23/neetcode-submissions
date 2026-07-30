7d90ba93333acf3ce4cff367d79325a0d7c13871
Basudev Mohapatra <166227341+basudevm23@users.noreply.github.com>
2026-07-30 16:11:02 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    
+public:
+    string encode(vector<string>& strs) {
+        string res = "";
+
+        for(auto s: strs){
+            res += to_string(s.length()) + "@" + s;
+        }
+
+        return res;
+    }
+
+    vector<string> decode(string s) {
+
+        int i = 0;
+        vector<string> res;
+
+        while(i < s.length()){
+            int j = i;
+
+            while(s[j]!='@'){
+                j++;
+            }
+            int number = stoi(s.substr(i, j-i));
+
+            res.push_back(s.substr(j+1, number));
+
+            i = j+1 + number;
+        }
+        return res;
+           
+    }
+};
