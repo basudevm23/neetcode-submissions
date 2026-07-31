732e0e3ec79f09e49ad108a9ba0a3d1562c137e0
Basudev Mohapatra <166227341+basudevm23@users.noreply.github.com>
2026-08-01 00:19:13 +0530

    Add: majority-element-ii - submission-4

---
+++ Data Structures & Algorithms/majority-element-ii/submission-4.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int num1 = -1;
+        int num2 = -1;
+
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for(int num: nums){
+            if(num == num1){
+                cnt1++;
+            }
+
+            else if(num == num2){
+                cnt2++;
+            }
+
+            else if(cnt1 == 0){
+                num1 = num;
+                cnt1 = 1;
+            }
+
+            else if(cnt2 == 0){
+                num2 = num;
+                cnt2 = 1;
+            }
+
+            else{
+                cnt1--;
+                cnt2--;
+            }
+
+        }
+
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for(int num: nums){
+            if(num == num1){
+                cnt1++;
+            }
+            else if(num == num2){
+                cnt2++;
+            }                                    
+        }
+
+        vector<int> res;
+
+        if(cnt1 > nums.size()/3){
+            res.push_back(num1);            
+        }
+
+        if(cnt2 > nums.size()/3){
+            res.push_back(num2);
+        }
+
+        return res;                
+    }
+};
